e38c080484a9ced9f201e595e87d5428aa6f1780
nadimahmed1 <nadimarafat26@gmail.com>
2022-08-13 12:18:26 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
 import logo from './logo.svg';
 import './App.css';
 
+const name = 'Nadim';
+const singer = { name: 'Dr Mahfuz', job: 'singer' }
+
+const singerStyle = {
+  color: 'yellow'
+}
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Person name="Nadim" nayika="mousumi"></Person>
+      <Person name="Ahmed" nayika="Kuyel Mallik"></Person>
+      <Person name="Shakil" nayika="sabnur"></Person>
+      <h5>New component!! Yeaaa</h5>
+      <Friend></Friend>
+      <Friend></Friend>
     </div>
   );
+}
+
+function Person(props) {
+
+  return (
+    <div className="person">
+      <h1>{props.name}</h1>
+      <p>Nayika: {props.nayika}</p>
+    </div>
+  )
+}
+
+function Friend(props) {
+  // console.log(props)
+  return (
+    <div className="habib">
+      <h3>Name: Habib Raj</h3>
+      <p>profession: Singer</p>
+    </div>
+  )
 }
 
 export default App;
